fe93550325be99ce9442b4ef6c1d9a6fcbae5950
csh8696-git <csh8696@gmail.com>
2026-01-06 10:15:43 +0900

    solution2_30
    가운데 글자 가져오기

---
+++ solution2.cpp
@@ -0,0 +1,36 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+	string answer = "";
+	if (s.length() % 2 != 0)
+	{
+		answer += s[s.length() / 2];
+	}
+	else
+	{
+		answer += s[s.length() / 2 - 1];
+		answer += s[s.length() / 2];
+	}
+	return answer;
+}
+
+string solution(string s) {
+	return s.length() & 1 ? s.substr(s.length() * 0.5, 1) : s.substr(s.length() * 0.5 - 1, 2);
+}
+string solution(string s) {
+	int len = s.length();
+	if (len % 2 == 0) {
+		return s.substr(len / 2 - 1, 2);
+	}
+	else {
+		return s.substr(len / 2, 1);
+	}
+}
+string solution(string s) {
+	int size = s.size();
+	string answer = s.substr((size - 1) / 2, (size - 1) % 2 + 1);
+	return answer;
+}
